58f7ea7c5a593eeae5ab38fcea7f14797641d76c
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-26 13:56:23 -0400

    put spacing in code to look better

--- part1-conditionals.js
+++ part1-conditionals.js
@@ -8,9 +8,9 @@ if (score > 90) {
     console.log("Grade A")
 } else if(score > 80) {
     console.log("Grade B")
-} else if(score>70){
+} else if(score > 70){
     console.log("Grade C")
-} else if(score>60){
+} else if(score > 60){
     console.log("Grade D")
 } else{
     console.log("Grade F")
