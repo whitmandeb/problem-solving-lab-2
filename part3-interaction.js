4d384f2e84053efd6e50ae41f851afb19823305a
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-26 22:02:23 -0400

    playing around with DOM from the demo

--- part3-interaction.js
+++ part3-interaction.js
@@ -6,7 +6,10 @@ function sayHello() {
     messageElement.innerHTML = "Hello";
 }
 
-
+function revertGreeting (){
+    let messageElement = document.getElementById ("message");
+    messageElement.innerHTML = "Click the button to change this text."
+}
 function showName() {  
     let nameInput = document.getElementById("nameInput").value;
     alert("Hello " + nameInput);
