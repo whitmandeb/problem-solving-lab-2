8bd7bb21184bc57701ece8f2a33937170e4ed48b
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-26 22:06:50 -0400

    formatted

--- part3-interaction.js
+++ part3-interaction.js
@@ -1,17 +1,15 @@
 //This code directly interacts with the web page.
 
-
 function sayHello() {
-    let messageElement = document.getElementById ("message");
-    messageElement.innerHTML = "Hello";
+  let messageElement = document.getElementById("message");
+  messageElement.innerHTML = "Hello";
 }
 
-function revertGreeting (){
-    let messageElement = document.getElementById ("message");
-    messageElement.innerHTML = "Click the button to change this text."
+function revertGreeting() {
+  let messageElement = document.getElementById("message");
+  messageElement.innerHTML = "Click the button to change this text.";
 }
-function showName() {  
-    let nameInput = document.getElementById("nameInput").value;
-    alert("Hello " + nameInput);
+function showName() {
+  let nameInput = document.getElementById("nameInput").value;
+  alert("Hello " + nameInput);
 }
-
