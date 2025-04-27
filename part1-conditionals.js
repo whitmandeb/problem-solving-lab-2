3196ec220f16e164c61f24f21a6c5e93c2bfd647
whitmandeb <debbie.whitman01@yahoo.com>
2025-04-26 23:16:35 -0400

    formatted

--- part1-conditionals.js
+++ part1-conditionals.js
@@ -1,4 +1,4 @@
-//Code must log to the browser console
+// 1. Grade Checker
 
 let score = 82;
 // Print A, B, C, D, or F based on score
@@ -16,12 +16,17 @@ if (score > 90) {
 }
 console.log(score);
 
+
+// 2. Pizza Quantity Warning
+
 let quantity = 12;
 // Print warning if quantity > 10
 if (quantity > 10) {
-  //Check the console on your browser
   console.log("Warning!");
 }
+
+
+// 3. Login Simulator
 
 let password = "letmein"; // or use prompt
 // Print "Access granted" or "Access denied"
